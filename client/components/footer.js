e719a9ca79095a09d419b5fda41789f35087e172
mWhitman189 <mwhitman189@gmail.com>
2020-12-23 23:23:26 +0900

    [task] Convert 'footer.js' to styled-components

--- client/components/footer.js
+++ client/components/footer.js
@@ -1,28 +1,25 @@
-import BREAKPOINTS from '../styles/breakpoints'
+import styled from 'styled-components'
 
+
+const Container = styled.footer`
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    border-top: 1px solid #eaeaea;
+    width: 100%;
+    height: 100px;
+`
+
+const Content = styled.div`
+
+`
 
 export default function Footer() {
     return (
-        <>
-            <div className="footer">
-                <footer>
-                    <div className="content">
-                        © {new Date().getFullYear()} Pera Pera Exchange
-                </div>
-                </footer>
-            </div>
-
-            <style jsx>{`
-                .footer {
-                    display: flex;
-                    justify-content: center;
-                    align-items: flex-end;
-                    border-top: 1px solid #eaeaea;
-                    width: 100%;
-                    height: 100px;
-                }
-            `}
-            </style>
-        </>
+        <Container>
+            <Content>
+                © {new Date().getFullYear()} Pera Pera Exchange
+            </Content>
+        </Container>
     )
 }
