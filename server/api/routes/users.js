2a0755269981ae5fb90b93ecb257a447b0f6ca05
mWhitman189 <mwhitman189@gmail.com>
2021-01-03 11:24:36 +0900

    [Task] Add final touches to landing page, without figure

--- server/api/routes/users.js
+++ server/api/routes/users.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const router = express.Router()
-const UsersController = require('../controllers/users')
+const express = require('express');
+const router = express.Router();
+const UsersController = require('../controllers/users');
 
-const checkAuth = require('../middleware/check-auth')
+const checkAuth = require('../middleware/check-auth');
 
 
-router.get('/', UsersController.users_get_all)
+router.get('/', UsersController.users_get_all);
 
-router.post('/signup', UsersController.users_sign_up)
+router.post('/signup', UsersController.users_sign_up);
 
-router.post('/login', UsersController.users_log_in)
+router.post('/login', UsersController.users_log_in);
 
 // Add endpoint for refreshing the accessToken
 // router.post('/token', checkAuth, UsersController.users_refresh_token)
-router.get('/token', UsersController.users_refresh_token)
+router.get('/token', UsersController.users_refresh_token);
 
-router.post('/logout', UsersController.users_log_out)
+router.post('/logout', UsersController.users_log_out);
 
-router.delete('/:userId', checkAuth, UsersController.users_delete_token)
+router.delete('/:userId', checkAuth, UsersController.users_delete_token);
 
-module.exports = router
+module.exports = router;
